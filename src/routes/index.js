ba84d3bbf95bf9dcbffbc9d9b30bbd566727893c
JYeon <doryJYeon@gmail.com>
2024-02-26 14:30:40 +0900

    [+] movie search, list, detail

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import { createRouter } from "../core/core"
-import Home from "../Home"
+import Home from "./Home"
+import Movie from "./Movie.js"
 
 export default createRouter([
-  { path: "#/", component: Home }
+  { path: "#/", component: Home },
+  { path: "#/movie", component: Movie }
 ])
